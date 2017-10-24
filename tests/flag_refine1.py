c4a38b782c5fe11c128aef087fa5f85b6a8178f7
Praveen C <cpraveen@gmail.com>
2017-10-24 13:24:12 +0530

    How to flag cells for refinement and their neighbor

---
+++ tests/flag_refine1.py
@@ -0,0 +1,48 @@
+"""
+1) Flag a cell and refine it.
+2) Flag and cell and its neighbors and refine
+"""
+from dolfin import *
+import matplotlib.pyplot as plt
+
+# Set-up mesh and connectivity
+mesh = UnitSquareMesh(5,5)
+tdim = mesh.topology().dim()
+mesh.init(tdim - 1, tdim)
+
+flag1 = CellFunction("bool", mesh)
+flag2 = CellFunction("bool", mesh)
+
+for cell in cells(mesh):
+    flag1[cell] = False
+    flag2[cell] = False
+
+# Loop over cells
+for cell in cells(mesh):
+    if cell.index() == 15:
+        flag1[cell] = True
+        flag2[cell] = True
+        for facet in facets(cell):
+            # Filtered list of neighbor cells
+            neighbor = filter(lambda ci: ci != cell.index(), 
+                              facet.entities(tdim))
+            ncell = Cell(mesh, neighbor[0])
+            flag2[ncell] = True
+
+mesh1 = adapt(mesh, flag1)
+mesh2 = adapt(mesh, flag2)
+
+plt.figure()
+plot(mesh)
+
+plt.figure()
+plot(mesh1)
+plot(flag1)
+plt.title("Flagged cell 15")
+
+plt.figure()
+plot(mesh2)
+plot(flag2)
+plt.title("Flagged cell 15 and its neighbors")
+
+plt.show()
