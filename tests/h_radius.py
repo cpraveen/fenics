e9d0c2196b477d81ffc064d87b41596f88843eed
Praveen C <cpraveen@gmail.com>
2018-04-06 08:59:28 +0530

    Example to show circumradius and inradius field construction

---
+++ tests/h_radius.py
@@ -0,0 +1,23 @@
+'''
+Construct a piecewise constant function which gives inradius and circumradius
+Works in parallel also.
+'''
+import numpy as np
+from dolfin import *
+
+mesh = UnitSquareMesh(5,5)
+
+# Iterate over cells to get circumradius/area/...
+h1 = []; h2 = []
+for cell in cells(mesh):
+    h1.append(cell.circumradius())
+    h2.append(cell.inradius())
+
+V = FunctionSpace(mesh,'DG',0)
+H1 = Function(V)
+H1.vector().set_local(np.array(h1))
+H2 = Function(V)
+H2.vector().set_local(np.array(h2))
+
+File('H1.pvd') << H1
+File('H2.pvd') << H2
